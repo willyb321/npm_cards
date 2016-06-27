d88824801ae9dbb3c65fe8b57e848d05c5336cd0
William Blythe <wbwilliam7@gmail.com>
2016-06-27 12:38:45 +1000

    bad me

--- cli.js
+++ cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 'use strict';
+const path = require('path');
+
 const opn = require('opn');
 
-const img = __dirname + "/cards.png"
+const img = path.join(__dirname, '/cards.png');
 console.log('opening...');
 opn(img);
